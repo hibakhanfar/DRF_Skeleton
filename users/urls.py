21bbfbf435a92c8e1895598a9c3f60a32879b0cc
Hiba Ibrahim Khanfar <hibaibraheem140@gmail.com>
2026-08-30 01:01:51 +0300

    Components

--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import RegisterView,UserProfileView
+from .views import *
 urlpatterns = [
+
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', UserProfileView.as_view(), name='user_profile'),
+
 ]
