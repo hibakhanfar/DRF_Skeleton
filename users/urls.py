a785ec485e26d7b2dbf6bc4bde79b09f3159b925
Hiba Ibrahim Khanfar <hibaibraheem140@gmail.com>
2026-08-30 15:37:00 +0300

    email1

--- users/urls.py
+++ users/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', UserProfileView.as_view(), name='user_profile'),
-    path('email-verify/', VerifyEmailView.as_view(), name="email-verify"),
     path('login/', CustomLoginView.as_view(), name='login'),
 ]
