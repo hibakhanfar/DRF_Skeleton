be5f18bbd8b59ea9a68e8383c42b885644f55a28
Hiba Ibrahim Khanfar <hibaibraheem140@gmail.com>
2026-08-31 11:19:21 +0300

    EMAIL

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import (
 )
 from .views import *
 urlpatterns = [
-
+path('email-verify/', VerifyEmailView.as_view(), name="email-verify"),
     path('register/', RegisterView.as_view(), name='register'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', UserProfileView.as_view(), name='user_profile'),
